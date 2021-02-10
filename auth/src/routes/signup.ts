d8d943f834706d9a309b542b96ede627b3f967a2
Shivam Kumar <shivam123425@gmail.com>
2021-02-10 22:19:35 +0530

    Finish /signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/User";
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
@@ -22,11 +23,19 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user");
+    // find if the user with the email already exists;
 
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError("Email already in use");
+    }
+
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).json(user);
   }
 );
 
