9e59a10702562d6a47da488e07e17e224d66caf2
Shivam Kumar <shivam123425@gmail.com>
2021-02-28 16:26:35 +0530

    Handle fetching current user data in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default function HomePage() {
-  return <h1>Home</h1>;
+import axios from "axios";
+
+function LandingPage({ currentUser }) {
+  // console.log(currentUser);
+  // axios.get("/api/users/currentuser");
+  console.log(currentUser);
+  return <h1>Landing page !!!</h1>;
 }
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      `http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser`,
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get(`/api/users/currentuser`);
+
+    return data;
+  }
+};
+
+export default LandingPage;
