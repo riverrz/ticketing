ceff423181e3e35d19bc05f9a2defdfc014cd84e
Shivam Kumar <shivam123425@gmail.com>
2021-03-19 16:33:50 +0530

    complete request handler for creating a ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import { requireAuth, validateRequest } from "@skgittix/common";
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
+import { Ticket } from "../models/Ticket";
 
 const router = express.Router();
 
@@ -14,8 +15,11 @@ router.post(
       .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+    await ticket.save();
+    res.status(201).send(ticket);
   }
 );
 
