70cb16fc0c1f34104f57fee7f834aeec23062f49
Shivam Kumar <shivam123425@gmail.com>
2021-03-19 16:49:53 +0530

    Create a request handler for show ticket route

---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,15 @@
+import { NotFoundError } from "@skgittix/common";
+import express, { Response, Request } from "express";
+import { Ticket } from "../models/Ticket";
+
+const router = express.Router();
+
+router.get("/api/tickets/:id", async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
