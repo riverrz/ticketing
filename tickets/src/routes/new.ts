edb899d32b95310cad0737b27c755dacf718258a
Shivam Kumar <shivam123425@gmail.com>
2021-03-21 17:41:39 +0530

    Introduce nats and publish ticket created event

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import { requireAuth, validateRequest } from "@skgittix/common";
 import { body } from "express-validator";
 import express, { Request, Response } from "express";
 import { Ticket } from "../models/Ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -19,6 +21,12 @@ router.post(
     const { title, price } = req.body;
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket._id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );
