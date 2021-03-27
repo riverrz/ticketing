20ac561c8c68a6dac17b7bf9468c79f83f654ca4
Shivam Kumar <shivam123425@gmail.com>
2021-03-27 22:11:27 +0530

    Add middlwares to the route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
 import express, { Response, Request } from "express";
+import { validateRequest, requireAuth } from "@skgittix/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [body("ticketId").isMongoId().withMessage("TicketId must be provided")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
