649ab296a0e3305ac77b9b5c0fa8176ce008ed4f
Shivam Kumar <shivam123425@gmail.com>
2021-03-31 21:50:53 +0530

    Finish up test for checking if event was fired

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/Order";
 import { Ticket } from "../../models/Tickets";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("throws a 401 when user is not signed in", async () => {
   await request(app).post("/api/orders").send({}).expect(401);
@@ -67,4 +68,16 @@ it("reserves a ticket", async () => {
   expect(response.body).toHaveProperty("id");
 });
 
-it.todo("emits an order created event");
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "Concert",
+    price: 20,
+  });
+  await ticket.save();
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
