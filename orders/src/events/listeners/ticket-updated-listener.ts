914d6543b98ee738e115fe17a9f873c709c0c6ec
Shivam Kumar <shivam123425@gmail.com>
2021-04-03 13:39:09 +0530

    Create an abstract findByEvent method

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -9,10 +9,8 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
     const { title, price, id } = data;
 
-    const ticket = await Ticket.findOne({
-      _id: data.id,
-      version: data.version - 1,
-    });
+    const ticket = await Ticket.findByEvent(data);
+
     if (!ticket) {
       throw new Error("Ticket not found");
     }
