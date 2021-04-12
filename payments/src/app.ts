04acd64c9b24820415ece53af92725e5c7549d65
Shivam Kumar <shivam123425@gmail.com>
2021-04-12 19:26:55 +0530

    Create boilerplate, k8 infra for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUser, errorHandler, NotFoundError } from "@skgittix/common";
+
+const app = express();
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    secure: process.env.NODE_ENV !== "test",
+    signed: false,
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
