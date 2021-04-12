8d406afbbc7bbf3f9c27354385301f49acb0bcf6
Shivam Kumar <shivam123425@gmail.com>
2021-04-12 16:17:07 +0530

    Create ExpirationCompleteListener

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,31 @@
+import { ExpirationCompleteEvent, Listener, Subjects } from "@skgittix/common";
+import { Message } from "node-nats-streaming";
+import { Order, OrderStatus } from "../../models/Order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { queueGroupName } from "./queue-group-name";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+    msg.ack();
+  }
+}
