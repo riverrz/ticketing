fca0be8447f0427fad38e4ce805f32987514e153
Shivam Kumar <shivam123425@gmail.com>
2021-04-12 15:42:57 +0530

    use proper delay for the order for expiring

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,7 +8,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
