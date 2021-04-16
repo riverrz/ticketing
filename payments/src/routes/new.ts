6ff88ad4235436ecdebc9dd838f5188cea9309d5
Shivam Kumar <shivam123425@gmail.com>
2021-04-17 02:19:14 +0530

    Introduce stripe in payments service and create a charge in newCharge request handler

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { Order } from "../models/Order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -29,6 +30,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
