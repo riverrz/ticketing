1892dbb6cad82568856fb0e97864afbef403cdda
Shivam Kumar <shivam123425@gmail.com>
2021-04-23 23:31:29 +0530

    Skaffold a form for creating a ticket

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,9 @@ function AppComponent({ Component, pageProps, currentUser }) {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component currentUser={currentUser} {...pageProps} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 }
