b4136cfcaa1cc4f7055a781f9ecb70e1812ac594
Shivam Kumar <shivam123425@gmail.com>
2021-04-24 02:00:21 +0530

    Add stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,25 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
-  const [timeLeft, setTimeLeft] = useState(0)
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
     const findTimeLeft = () => {
       const msLeft = new Date(order.expiresAt) - new Date();
-      setTimeLeft(Math.round(msLeft/1000))
-    }
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
     findTimeLeft();
-    const timerId = setInterval(findTimeLeft, 1000)
-    return () => clearInterval(timerId)
-  }, [order])
-  
-  if(timeLeft < 0) {
-    return <div>Order expired</div>
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => clearInterval(timerId);
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order expired</div>;
   }
 
   return (
     <div>
       Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51IgyMwSBXvg4mDuaOFYl7uwPYZB0qaRqLZDWd7y9WJ9hkadNF1bN80j241Of7Mek64dHu7Q7qXdY6TwKKb2ne0Dq00FtNOCABR"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+        currency="INR"
+      />
     </div>
   );
 };
