f267236cae63d6b3c8193e24b7ae8c5b18055bf3
Shivam Kumar <shivam123425@gmail.com>
2021-04-24 02:17:45 +0530

    Add more nav links

--- client/components/header.js
+++ client/components/header.js
@@ -11,6 +11,14 @@ export default function Header({ currentUser }) {
       href: "/auth/signin",
     },
     currentUser && {
+      label: "Sell Tickets",
+      href: "/tickets/new",
+    },
+    currentUser && {
+      label: "My Orders",
+      href: "/orders",
+    },
+    currentUser && {
       label: "Sign out",
       href: "/auth/signout",
     },
