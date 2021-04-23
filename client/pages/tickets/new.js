2c35c8b92e7a6f5b430990006afddccbf0198aac
Shivam Kumar <shivam123425@gmail.com>
2021-04-23 23:40:41 +0530

    re route to landing page on successfull ticket creation

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Router from "next/router";
+
 import useRequest from "../../hooks/use-request";
 
 const NewTicket = () => {
@@ -11,7 +13,7 @@ const NewTicket = () => {
       title,
       price,
     },
-    onSuccess: (ticket) => console.log(ticket),
+    onSuccess: () => Router.push("/"),
   });
 
   const onBlur = () => {
