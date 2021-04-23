885d99d852779996724cf364dda26786f76c0e9c
Shivam Kumar <shivam123425@gmail.com>
2021-04-23 23:21:18 +0530

    minor refactoring

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from "../api/build-client";
-
 function LandingPage({ currentUser }) {
   return currentUser ? (
     <h1>You are signed in</h1>
@@ -8,10 +6,8 @@ function LandingPage({ currentUser }) {
   );
 }
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
